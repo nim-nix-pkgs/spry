39700faa9233c17f43fa009e2f1020e209f4beb4
hugosenari <hugosenari@gmail.com>
2022-03-12 04:16:27 -0300

    chore: re index spry

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A Smalltalk and Rebol inspired language implemented as an AST interpreter'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."spry-master".url = "path:./master";
+  inputs."spry-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."spry-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''A Smalltalk and Rebol inspired language implemented as an AST interpreter'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-spry-master.flake = false;
+  inputs.src-spry-master.owner = "gokr";
+  inputs.src-spry-master.ref   = "refs/heads/master";
+  inputs.src-spry-master.repo  = "spry";
+  inputs.src-spry-master.type  = "github";
+  
+  inputs."spryvm".dir   = "nimpkgs/s/spryvm";
+  inputs."spryvm".owner = "riinr";
+  inputs."spryvm".ref   = "flake-pinning";
+  inputs."spryvm".repo  = "flake-nimble";
+  inputs."spryvm".type  = "github";
+  inputs."spryvm".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."spryvm".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-spry-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-spry-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
